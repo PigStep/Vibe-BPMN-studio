232f14b552e3167892250c44e3999d31e75bec18
VlaDDOS <91902697+PigStep@users.noreply.github.com>
2025-12-14 23:45:45 +0300

    agent infra testing
    refactor: refactor LLM client into class and add BPMN testing infrastructure
    
    designed basic pipline in notebook

--- src/ai_generation/llm_client.py
+++ src/ai_generation/llm_client.py
@@ -1,32 +1,41 @@
 from openai import OpenAI
-import os
-from dotenv import load_dotenv
 from settings import get_settings
 
 settings = get_settings()
 
-
 AI_API_KEY = settings.OPENROUTER_API_KEY
 MODEL_NAME = settings.OPENROUTER_MODEL_NAME
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=AI_API_KEY,
-)
 
-completion = client.chat.completions.create(
-    extra_headers={
-        "HTTP-Referer": "<YOUR_SITE_URL>",  # Optional. Site URL for rankings on openrouter.ai.
-        "X-Title": "<YOUR_SITE_NAME>",  # Optional. Site title for rankings on openrouter.ai.
-    },
-    extra_body={},
-    model=MODEL_NAME,
-    messages=[{"role": "user", "content": "What is the meaning of life?"}],
-    stream=True,
-)
-full_response = ""
-for chunk in completion:
-    if chunk.choices[0].delta.content:
-        full_response += chunk.choices[0].delta.content
-        print(chunk.choices[0].delta.content, end="", flush=True)
-# print(completion.choices[0].message.content)
+class LLMClient:
+    def __init__(self, client: OpenAI, model_name: str, behaivour_promt: str):
+        self.client = client
+        self.model_name = model_name
+        self.behaivour_promt = behaivour_promt
+
+    def generate_response_json_based(self, prompt: str, json_schema: dict) -> str:
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "system", "content": self.behaivour_promt},
+                {"role": "user", "content": prompt},
+            ],
+            response_format={
+                "type": "json_schema",
+                "json_schema": {
+                    "name": "response_schema",
+                    "schema": json_schema,
+                },
+            },
+        )
+        return response.choices[0].message.content
+
+    def generate_response_text_based(self, prompt: str) -> str:
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "system", "content": self.behaivour_promt},
+                {"role": "user", "content": prompt},
+            ],
+        )
+        return response.choices[0].message.content
