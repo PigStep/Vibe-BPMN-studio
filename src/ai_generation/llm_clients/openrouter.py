ee33a6f1564f2622c3b9031f282c3dd404f42b9d
VlaDDOS <91902697+PigStep@users.noreply.github.com>
2026-04-02 20:44:35 +0300

    chore: close suppoort for class

--- src/ai_generation/llm_clients/openrouter.py
+++ src/ai_generation/llm_clients/openrouter.py
@@ -1,25 +1,26 @@
-from typing import Literal
+from typing import Literal, Any
 from openai import OpenAI
 from settings import get_settings
-from src.ai_generation.llm_clients.llm_base import LLMClient
+from src.ai_generation.llm_clients.llm_base import LLMClient, SToolCall
+from src.ai_generation.managers.tool.manager_base import ToolManager
+from src.ai_generation.managers.tool.openrouter import OpenAIToolManager
 from pydantic import BaseModel
 
 
 class OpenRouterClient(LLMClient):
     def __init__(self):
+        self.tool_manager: ToolManager | None = None
         self._setup_client()
 
     def _setup_client(self):
         settings = get_settings()
-
         AI_API_KEY = settings.OPENROUTER_API_KEY
         self.model_name = settings.OPENROUTER_MODEL_NAME
-
-        raw_client = OpenAI(
+        self.client = OpenAI(
             api_key=AI_API_KEY,
             base_url="https://openrouter.ai/api/v1",
         )
-        self.client = raw_client
+        self.tool_manager = OpenAIToolManager()
 
     def _generate_response(
         self,
@@ -28,49 +29,23 @@ class OpenRouterClient(LLMClient):
         temperature: float | None = None,
         response_format: dict | None = None,
         extra_body: dict | None = None,
-    ) -> str | None:
-        """
-        Generate a response from the LLM using the provided prompt.
-
-        Parameters
-        ----------
-        prompt : str
-            The user prompt to send to the model.
-        system_prompt : str
-           The system prompt to send as the initial message to the model.
-        temperature : float, optional
-            Sampling temperature to use for response generation. Default is 0.7.
-        response_format : dict | None, optional
-            Response format specification. Example:
-            ```
-            {
-                "type": "json_schema",
-                "json_schema": {
-                    "name": "response_schema",
-                    "schema": json_schema,
-                }
-            }
-            ```
-        extra_body : dict | None, optional
-            Additional key/value pairs to be included in the request body.
-            Example: {'reasoning': {'effort': 'high'}}.
-
-        Returns
-        -------
-        str | None
-            Content of the first message in the model's response, or None if no content is present.
-        """
+        tools: list[dict[str, Any]] | None = None,
+        tool_choice: str | None = None,
+    ) -> Any:
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt},
+        ]
         response = self.client.chat.completions.create(
             model=self.model_name,
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": prompt},
-            ],
+            messages=messages,
             temperature=temperature,
             response_format=response_format if response_format else None,
             extra_body=extra_body if extra_body else None,
+            tools=tools,
+            tool_choice=tool_choice if tool_choice else None,
         )
-        return response.choices[0].message.content
+        return response
 
     def generate_response_json_based(
         self,
@@ -80,34 +55,12 @@ class OpenRouterClient(LLMClient):
         reasoning_mode: Literal["none", "minimal", "low", "medium", "high"] = "none",
         temperature: float | None = None,
     ) -> str | None:
-        """
-        Generates a JSON-formatted response from the LLM based on the provided prompt and schema.
-
-        Parameters
-        ----------
-        prompt : str
-            The user prompt to be sent to the model.
-        json_schema : dict | BaseModel
-            A JSON Schema dict or Pydantic BaseModel that defines the expected structure of the model's output.
-        system_prompt : str
-            The system prompt to be sent as the initial message to the model.
-        reasoning_mode : Literal["none", "minimal", "low", "medium", "high"], optional
-            The level of reasoning effort the model should apply when generating the response.
-            Defaults to "none".
-        temperature : float | None, optional
-            Sampling temperature for response generation.
-
-        Returns
-        -------
-        str | None
-            The JSON response as a string if the model returns a valid response; otherwise, None.
-        """
         schema = (
             json_schema.model_json_schema()
             if isinstance(json_schema, type) and issubclass(json_schema, BaseModel)
             else json_schema
         )
-        return self._generate_response(
+        response = self._generate_response(
             prompt=prompt,
             system_prompt=system_prompt,
             response_format={
@@ -120,6 +73,7 @@ class OpenRouterClient(LLMClient):
             extra_body={"reasoning": {"effort": reasoning_mode}},
             temperature=temperature,
         )
+        return response.choices[0].message.content
 
     def generate_response_text_based(
         self,
@@ -128,13 +82,59 @@ class OpenRouterClient(LLMClient):
         reasoning_mode: Literal["none", "minimal", "low", "medium", "high"] = "none",
         temperature: float | None = None,
     ) -> str | None:
-        """
-        Generates a text-formatted response from the LLM based on the provided prompt.
-        Offer a choice of reasoning effort levels.
-        """
-        return self._generate_response(
-            prompt,
-            system_prompt,
+        response = self._generate_response(
+            prompt=prompt,
+            system_prompt=system_prompt,
             extra_body={"reasoning": {"effort": reasoning_mode}},
             temperature=temperature,
         )
+        return response.choices[0].message.content
+
+    def _proceed_response(self, response) -> SToolCall:
+        """
+        Assemble LLM tool output to class
+        """
+        message = response.choices[0].message
+        tool_calls: list[SToolCall] | None = None
+        text: str | None = None
+        finish_reason = (
+            str(response.choices[0].finish_reason)
+            if response.choices[0].finish_reason
+            else None
+        )
+
+        if message.tool_calls:
+            tool_calls = []
+            for tc in message.tool_calls:
+                tool_calls.append(
+                    SToolCall(name=tc.function.name, arguments=tc.function.arguments)
+                )
+
+        if message.content:
+            text = message.content
+
+        return SToolCall(text=text, tool_calls=tool_calls, finish_reason=finish_reason)
+
+    def generate_tool_call(
+        self,
+        prompt: str,
+        system_prompt: str,
+        tools: list[Any] | None = None,
+        reasoning_mode: Literal["none", "minimal", "low", "medium", "high"] = "none",
+        temperature: float | None = None,
+    ) -> SToolCall:
+        if tools:
+            self.tool_manager.save_tools(tools)
+            tools_config = self.tool_manager.get_tools()
+        else:
+            tools_config = None
+
+        response = self._generate_response(
+            prompt=prompt,
+            system_prompt=system_prompt,
+            extra_body={"reasoning": {"effort": reasoning_mode}},
+            tools=tools_config,
+            temperature=temperature,
+        )
+
+        return self._proceed_response(response)
