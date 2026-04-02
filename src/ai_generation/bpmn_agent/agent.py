f47ba2a115b317dd76a18a803ebc1ec0beb58dc9
VlaDDOS <91902697+PigStep@users.noreply.github.com>
2026-04-02 21:01:36 +0300

    feat: switch to ReAct model

--- src/ai_generation/bpmn_agent/agent.py
+++ src/ai_generation/bpmn_agent/agent.py
@@ -1,57 +1,62 @@
-from langgraph.graph import START, END, StateGraph
-from functools import partial
-
-from src.ai_generation.llm_clients import get_llm_client
+from settings import settings
+from langchain_classic.schema import AIMessage
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.messages import HumanMessage
+from src.ai_generation.llm_clients.llm_base import LLMClient
 from src.ai_generation.managers.llm_config import LLMConfigManager
-from src.ai_generation.bpmn_agent.state import SimpleBPMNAgent
-from src.ai_generation.bpmn_agent.nodes.get_bpmn_node import generate_bpmn
-from src.ai_generation.bpmn_agent.nodes.imagine_procces_node import generate_process
+from src.ai_generation.bpmn_agent.tools.generate_bpmn_draft import generate_draft
+from src.ai_generation.llm_clients import get_llm_client, get_langgraph_llm_client
 from src.schemas import SUserInputData
 
 
-def build_bpmn_agent() -> StateGraph:
-    # Define managers and LLM client
-    llm = get_llm_client()
-    prompt_manager = LLMConfigManager(r"data/prompts/")
-    agent_builder = StateGraph(SimpleBPMNAgent)
+def get_dependencies() -> tuple[LLMConfigManager, LLMClient, BaseChatModel]:
+    config_manager = LLMConfigManager(r"data/prompts/")
+    llm = get_langgraph_llm_client()
+    non_langgraph_llm = get_llm_client()
 
-    # Define node with partial
-    generate_process_with_config = partial(
-        generate_process,
-        llm=llm,
-        configuration=prompt_manager.get_call_config("business_generation"),
+    llm_with_tools = llm.bind_tools([generate_draft])
+
+    return (config_manager, non_langgraph_llm, llm_with_tools)
+
+
+def _run_tools(
+    response: AIMessage,
+    non_langgraph_llm: LLMClient,
+    config_manager: LLMConfigManager,
+    session_id: str,
+):
+    for tool_call in response.tool_calls:
+        result = generate_draft.invoke(
+            tool_call["args"],
+            config={
+                "configurable": {
+                    "llm": non_langgraph_llm,
+                    "config_manager": config_manager,
+                    "session_id": session_id,
+                }
+            },
+        )
+        # FIXME: 1 tool for now - return first tool
+        return result
+
+
+def get_agent_answer(
+    user_input: SUserInputData,
+    llm_with_tools: BaseChatModel,
+    non_langgraph_llm: LLMClient,
+    config_manager: LLMConfigManager,
+):
+    response = llm_with_tools.invoke([HumanMessage(content=user_input.user_input)])
+    return _run_tools(
+        response, non_langgraph_llm, config_manager, user_input.session_id
     )
-    generate_bpmn_with_config = partial(
-        generate_bpmn,
-        llm=llm,
-        configuration=prompt_manager.get_call_config("XML_generation"),
-    )
-
-    # Build workflow
-    agent_builder.add_node("imagine", generate_process_with_config)
-    agent_builder.add_node("generate", generate_bpmn_with_config)
-
-    agent_builder.add_edge(START, "imagine")
-    agent_builder.add_edge("imagine", "generate")
-    agent_builder.add_edge("generate", END)
-
-    return agent_builder
-
-
-_agent = None
-
-
-def get_agent_answer(initial_state: dict) -> dict:
-    global _agent
-    if _agent is None:
-        _agent = build_bpmn_agent().compile()
-    result = _agent.invoke(initial_state)
-    return result
 
 
 def invoke_agent(user_input: SUserInputData) -> str:
-    initial_state = {
-        "user_input": user_input.user_input,
-        "previous_stage": "",
-    }
-    return get_agent_answer(initial_state)
+    config_manager, non_langgraph_llm, llm_with_tools = get_dependencies()
+    return get_agent_answer(
+        user_input,
+        llm_with_tools,
+        non_langgraph_llm,
+        config_manager,
+    )
