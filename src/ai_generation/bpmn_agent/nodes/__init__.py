b485f4d8c6b7f82bd8b8f4c9055e16a854dec1c0
VlaDDOS <91902697+PigStep@users.noreply.github.com>
2026-04-09 18:31:32 +0300

    fix(inport): fix 'src' in import path

--- src/ai_generation/bpmn_agent/nodes/__init__.py
+++ src/ai_generation/bpmn_agent/nodes/__init__.py
@@ -1,2 +1,2 @@
-from ai_generation.bpmn_agent.nodes.generate_xml import generate_xml
+from src.ai_generation.bpmn_agent.nodes.generate_xml import generate_xml
 from src.ai_generation.bpmn_agent.nodes.imagine_procces_node import generate_process
