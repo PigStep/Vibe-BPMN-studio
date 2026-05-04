aebac5c939756384f2554c0943fb7b9d257d5702
VlaDDOS <91902697+PigStep@users.noreply.github.com>
2026-05-04 21:57:16 +0300

    feat: add openAI support

--- src/ai_generation/bpmn_agent/langgraph.py
+++ src/ai_generation/bpmn_agent/langgraph.py
@@ -1,6 +1,7 @@
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.language_models.chat_models import BaseChatModel
+from pydantic import Secret, SecretStr
 from settings import settings
 
 _llm: BaseChatModel | None = None
@@ -13,14 +14,33 @@ def get_langgraph_llm_client() -> BaseChatModel:
     if _llm is None:
         match provider:
             case "gemini":
+                model = settings.OPENROUTER_MODEL_NAME
+                key = settings.OPENROUTER_API_KEY
+
+                if model is None or key is None:
+                    raise ValueError(
+                        f"Enviroment model and api_key were not set up. Check .env"
+                    )
+
                 _llm = ChatGoogleGenerativeAI(
-                    model=settings.GEMINI_MODEL_NAME,
-                    max_retries=2,
-                    api_key=settings.GEMINI_API_KEY,
+                    model=model,
+                    max_retries=7,
+                    api_key=SecretStr(key),
                 )
             case "openrouter":
-                # TODO: implement support
-                _llm = ChatOpenAI()
+                model = settings.OPENROUTER_MODEL_NAME
+                key = settings.OPENROUTER_API_KEY
+
+                if model is None or key is None:
+                    raise ValueError(
+                        f"Enviroment model and api_key were not set up. Check .env"
+                    )
+
+                _llm = ChatOpenAI(
+                    model=model,
+                    max_retries=7,
+                    api_key=SecretStr(key),
+                )
             case _:
                 raise ValueError(f"Unknown provider: {provider}")
     return _llm
