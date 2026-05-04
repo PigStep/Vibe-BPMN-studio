a6faf6660f1bee96267c7959ad812a830e509637
VlaDDOS <91902697+PigStep@users.noreply.github.com>
2026-05-04 21:51:49 +0300

    fix: fixed critical memory handling bug
    because of using and modifing global dict there was a bug for generating responses

--- src/ai_generation/bpmn_agent/nodes/generate_xml.py
+++ src/ai_generation/bpmn_agent/nodes/generate_xml.py
@@ -12,18 +12,18 @@ def generate_xml(
     state: AgentState, llm: BaseChatModel, configuration: dict
 ) -> AgentState:
     """Generates XML code from instructions"""
-    system = configuration.pop("system_prompt", None)
+    # Using copy because working with global object (partial)
+    config_copy = configuration.copy()
+    system = config_copy.pop("system_prompt", None)
     messages = state["messages"]
     if system:
         messages = [SystemMessage(content=system)] + messages
-    llm = llm.bind(**configuration)
+    xml_response = llm.bind(**config_copy).invoke(messages)
+    xml_content = xml_response.text
     logger.info("Session %s. Generating XML", state["session_id"])
-    xml_content = llm.invoke(messages)
-    # FIXME: return DEBUG - Agent was interupted. Interrupt message: [Interrupt(value={'xml_result': ''}, id='78b2a0fb7b787961739438f6d1884e94')]
-    # At the second iteration. Find why
-    user_feedback = interrupt({"xml_result": xml_content.text})
+    user_feedback = interrupt({"xml_result": xml_content})
     return {
-        "messages": [
+        "messages": [  # type: ignore
             AIMessage(content=xml_content),
             HumanMessage(content=user_feedback),
         ]
--- src/ai_generation/bpmn_agent/nodes/imagine_procces_node.py
+++ src/ai_generation/bpmn_agent/nodes/imagine_procces_node.py
@@ -11,12 +11,14 @@ def generate_process(
     state: AgentState, llm: BaseChatModel, configuration: dict
 ) -> AgentState:
     """Generate business process as plan for given instructions"""
-    system = configuration.pop("system_prompt", None)
+    # Using copy because working with global object (partial)
+    config_copy = configuration.copy()
+    system = config_copy.pop("system_prompt", None)
     messages = state["messages"]
     if system:
         messages = [SystemMessage(content=system)] + messages
 
     logger.info("Session %s. Generating BPMN business procces", state["session_id"])
-    llm = llm.bind(**configuration)
-    result = llm.invoke(messages)
-    return {"messages": [result]}
+    result = llm.bind(**config_copy).invoke(messages)
+
+    return {"messages": [result]}  # type: ignore
